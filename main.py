a46fd47c58585173e8f3b8f48a166aa587f8197b
std46d6b <dev@m5k.ru>
2024-09-21 20:57:08 +0300

    g1-v2

--- main.py
+++ main.py
@@ -15,24 +15,39 @@ t = np.linspace(0, t_max, t_max * fps)
 x = v * t - r * np.sin(omega * t)
 y = r - r * np.cos(omega * t)
 
+theta = np.linspace(0, 2 * np.pi, t_max * fps)
+circle_x = r * np.cos(theta)
+circle_y = r * np.sin(theta) + r
+
 fig, ax = plt.subplots()
 ax.set_xlim(0, max(x))
 ax.set_ylim(0, 2 * r * 1.1)
 
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+
+ax.set_aspect('equal', 'box')
+
 line, = ax.plot([], [], 'b-', lw=2)
 point, = ax.plot([], [], 'r.')
+
+wheel, = ax.plot([], [], 'g-', lw=1)
 
 
 def init():
     line.set_data([], [])
     point.set_data([], [])
-    return line, point
+    wheel.set_data([], [])
+    return line, point, wheel
 
 
 def update(frame):
     line.set_data(x[:frame], y[:frame])
     point.set_data([x[frame]], [y[frame]])
-    return line, point
+
+    wheel.set_data(v * t[frame] + circle_x, circle_y)
+
+    return line, point, wheel
 
 
 ani = animation.FuncAnimation(fig, update, frames=len(t), init_func=init, blit=True, interval=1000 / fps)
